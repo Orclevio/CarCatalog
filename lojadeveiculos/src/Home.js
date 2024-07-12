bcadea1ba35598d0c0be2de3b1084fc7c78b0631
Orclevio Alves <34605847+Orclevio@users.noreply.github.com>
2024-07-12 00:30:18 -0300

    Criação home e salvamento das imagens byd

---
+++ lojadeveiculos/src/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+import imgprinc from './imagens/tesla_x_0.jpg';
+function Home() {
+    return (
+      <div className="Home">
+        <header className="Home-header">
+         
+        </header>
+        <img src={imgprinc} alt="Car" />
+      </div>
+    );
+  }
+  
+  export default Home;
